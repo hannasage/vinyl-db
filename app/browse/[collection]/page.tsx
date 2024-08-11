ab41f23d0a29339b1aceba64871e495f104c4edf
kevinhaube <4022304+kevinhaube@users.noreply.github.com>
2024-08-10 22:21:24 -0400

    update servier/client utils

--- app/browse/[collection]/page.tsx
+++ app/browse/[collection]/page.tsx
@@ -4,6 +4,7 @@ import { createSupabaseClient } from '@/utils/supabase/createSupabaseClient';
 import { SortType, sortLegacyEntries } from '@/data/filters';
 import { getFullList } from '@/utils/supabase/queries';
 import { FullAlbumDetails } from '@/data/types';
+import Pill from '@/components/atom/Pill';
 
 export default async function Page({ params }: { params: { collection: SortType['slug'] } }) {
   const sb = createSupabaseClient()
@@ -12,7 +13,10 @@ export default async function Page({ params }: { params: { collection: SortType[
 
   return (
     <main className="flex min-h-screen flex-col items-start my-8 mx-2 lg:mx-6">
-      {<AlbumGrid albums={(sortedAlbums as FullAlbumDetails[])} />}
+      {/*<div className="flex items-start w-full top-0 sticky z-10 bg-gradient-to-b from-black">*/}
+      {/*  <Pill text={"D"} className={"px-6 my-6"}/>*/}
+      {/*</div>*/}
+      <AlbumGrid albums={(sortedAlbums as FullAlbumDetails[])} />
     </main>
   );
 }
