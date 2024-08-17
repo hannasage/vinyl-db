9665a08c8381ffe83e8e475640a6a8896d08a5c7
kevinhaube <4022304+kevinhaube@users.noreply.github.com>
2024-08-17 10:19:34 -0400

    fix lint

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,6 @@ import React from 'react';
 import NewestAlbumsScroller from '@/components/organism/NewestAlbumsScroller';
 import CollectionGrid from '@/components/organism/CollectionGrid';
 
-// export async function NewestCarousel() {
-//   const sb = createClient()
-//   const { data, error } = await sb.functions.invoke('get-album-list', { body: {
-//       list: "newest"
-//     }});
-//   if (error) redirect('/error')
-//   return (
-//     <section className={"flex flex-row"}>
-//
-//     </section>
-//   )
-// }
-
 export default async function Page() {
   return (
     <main className="flex min-h-screen flex-col items-start my-8">
--- components/organism/NewestAlbumsScroller.tsx
+++ components/organism/NewestAlbumsScroller.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import React from 'react';
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
