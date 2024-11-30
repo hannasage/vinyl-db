c7cc0dd733f9cd90bca5a5a6408e012887837997
kevinhaube <4022304+kevinhaube@users.noreply.github.com>
2024-11-29 22:55:11 -0500

    finish link box

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import type { Metadata } from 'next';
-import { Inter } from "next/font/google";
+import { Anybody } from "next/font/google";
 import "./globals.css";
 import DotMatrixBackground from '@/components/DotMatrixBackground';
 import { LinkBoxBaseProps } from '@/components/boxes';
 import { MatrixBox } from '@/components/MatrixBox';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Anybody({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "some chick's vinyl collection",
@@ -29,9 +29,14 @@ export default async function RootLayout({
           <MatrixBox type={"link"} size={0} props={{
             href: "/v2",
             type: "nav",
+            text: "back",
+            arrowDirection: "left",
+            arrowSide: "left",
+            center: true,
           } as LinkBoxBaseProps} />
         </nav>
-        <main className={"absolute top-0 bottom-0 left-0 right-0"}>
+        {/*<main className={"absolute top-0 bottom-0 left-0 right-0"}>*/}
+        <main>
           {children}
         </main>
         <nav className={"absolute bottom-0"}>
--- components/boxes/LinkBox.tsx
+++ components/boxes/LinkBox.tsx
@@ -6,6 +6,7 @@ import classNames from 'classnames';
 export type LinkBoxBaseProps = LinkProps & {
   arrowDirection: "up" | "down" | "left" | "right"
   arrowSide: "left" | "right"
+  center: boolean,
   color: "dark" | "light"
   text: string
   type: "nav" | "box"
@@ -13,21 +14,41 @@ export type LinkBoxBaseProps = LinkProps & {
 
 export const LinkBox = ({
   size,
+  arrowSide,
+  arrowDirection,
+  center,
   className,
   color = "dark",
   href,
   text = "Link",
   type = "box"
 }: MatrixBoxBaseProps & LinkBoxBaseProps) => {
-  return(
-    <div className={classNames(className, {
-      [`flex`]: type === "nav",
+  const arrows: Record<LinkBoxBaseProps["arrowDirection"], string> = {
+    right: "→",
+    left: "←",
+    up: "↑",
+    down: "↓"
+  }
+  const Arrow = () => (
+    <span className={classNames({
+      ["mr-4"]: arrowSide === "left",
+      ["ml-4"]: arrowSide === "right"
+    })}>
+      {arrows[arrowDirection]}
+    </span>
+  );
+  return (
+    <div className={classNames(className, "no-underline", {
+      [`flex w-[100vw] p-6 text-white text-3xl tracking-wide`]: type === "nav",
+      ['justify-center align-middle']: center,
       [`w-[${size}%] h-[${size}%]`]: type === "box",
       ['bg-brandDarkGray']: color === "dark",
       ['bg-brandLightGray']: color === "light",
     })}>
       <Link href={href}>
+        {arrowSide === "left" && <Arrow />}
         {text}
+        {arrowSide === "right" && <Arrow />}
       </Link>
     </div>
   )
